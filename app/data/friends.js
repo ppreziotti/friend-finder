43c2f029fc5b83eafd89c6282706c687f66efed4
ppreziotti <patpreziotti@gmail.com>
2017-05-15 20:12:01 -0400

    link updates, started friend-matching logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,27 @@
 var friends = [{
-	name: ,
-	photo: ,
-	scores: []
+	name: "Ted Mosby",
+	photo: "../images/ted_mosby.jpg",
+	scores: ["3", "4", "5", "3", "5", "5", "2", "5", "5", "4"]
+},
+{
+	name: "Barney Stinson",
+	photo: "../images/barney_stinson",
+	scores: ["5", "1", "2", "5", "1", "3", "5", "1", "1", "5"]
+},
+{
+	name: "Robin Scherbatsky",
+	photo: "../images/robin_scherbatsky",
+	scores: ["4", "2", "3", "3", "3", "1", "2", "2", "1", "5"]
+},
+{
+	name: "Marshall Eriksen",
+	photo: "../images/marshall_eriksen",
+	scores: ["3", "4", "3", "2", "4", "5", "1", "5", "4", "2"]
+},
+{
+	name: "Lily Aldrin",
+	photo: "../images/lily_aldrin",
+	scores: ["2", "3", "3", "2", "3", "1", "2", "2", "4", "4"]
 }];
+
+module.exports = friends;
