3e2aaba052907572469be58d79ac5f4efd683ef8
ppreziotti <patpreziotti@gmail.com>
2017-05-17 19:43:17 -0400

    added friends post route

--- app/public/javascript/script.js
+++ app/public/javascript/script.js
@@ -1,4 +1,5 @@
 $("#submit").on("click", function() {
+	event.preventDefault();
 	var answers = document.getElementsByName("answer");
 	var userScores = [];
 	for (var i = 0; i < answers.length; i++) {
@@ -22,60 +23,61 @@ $("#submit").on("click", function() {
 			photo: $("#photo").val().trim(),
 			scores: userScores
 		}
+	}
 
-		console.log(newFriend);
+	console.log(newFriend);
 
-		var currentURL = window.location.origin;
-		$.ajax({
-			url: currentURL + "/api/friends", 
-			method: "GET"
-		}).done(function(data) {
-			var scoreDifArray = [];
-			for (var i = 0; i < data.length; i++) {
-				var scoreDif = 0;
-				for (var j = 0; j < data[i].scores.length; j++) {
-					scoreDif += Math.abs(parseInt(userScores[j]) - parseInt(data[i].scores[j]));
-				}
-				console.log(scoreDif);
-				scoreDifArray.push(scoreDif);
+	var currentURL = window.location.origin;
+	$.ajax({
+		url: currentURL + "/api/friends", 
+		method: "GET"
+	}).done(function(data) {
+		var scoreDifArray = [];
+		for (var i = 0; i < data.length; i++) {
+			var scoreDif = 0;
+			for (var j = 0; j < data[i].scores.length; j++) {
+				scoreDif += Math.abs(parseInt(userScores[j]) - parseInt(data[i].scores[j]));
 			}
-			console.log(scoreDifArray);
-			var minScoreDif = Math.min.apply(null, scoreDifArray);
-			console.log(minScoreDif);
-			var matchIndex = scoreDifArray.indexOf(minScoreDif);
-			var match = data[matchIndex];
-			console.log(match.name);
-			showModal();
+			console.log(scoreDif);
+			scoreDifArray.push(scoreDif);
+		}
+		console.log(scoreDifArray);
+		var minScoreDif = Math.min.apply(null, scoreDifArray);
+		console.log(minScoreDif);
+		var matchIndex = scoreDifArray.indexOf(minScoreDif);
+		var match = data[matchIndex];
+		console.log(match.name);
 
-			$.post("/api/friends", newFriend, function(data) {
-				//
+		$.post("/api/friends", newFriend).done(function(data) {
+			console.log(data);
+		});
+
+		showModal();
+
+		function showModal() {
+			$("#name").val("");
+			$("#photo").val("");
+			for (var i = 0; i < answers.length; i++) {
+				if (answers[i].checked) {
+					answers[i].checked = false;
+				}
+			}
+			var modal = document.getElementById('myModal');
+			$("#modalPar").html("You matched with " + match.name + "!");
+			$("#matchImg").attr("src", match.photo);
+			$("#matchImg").attr("alt", match.name);
+			modal.style.display = "block";	
+			// When the user clicks on <span> (x), close the modal
+			$("#close").on("click", function() {
+			    modal.style.display = "none";
 			});
 
-			function showModal() {
-				$("#name").val("");
-				$("#photo").val("");
-				for (var i = 0; i < answers.length; i++) {
-					if (answers[i].checked) {
-						answers[i].checked = false;
-					}
-				}
-				var modal = document.getElementById('myModal');
-				$("#modalPar").html("You matched with " + match.name + "!");
-				$("#matchImg").attr("src", match.photo);
-				$("#matchImg").attr("alt", match.name);
-				modal.style.display = "block";	
-				// When the user clicks on <span> (x), close the modal
-				$("#close").on("click", function() {
-				    modal.style.display = "none";
-				});
-
-				// When the user clicks anywhere outside of the modal, close it
-				window.onclick = function(event) {
-				    if (event.target == modal) {
-				        modal.style.display = "none";
-				    }
-				}
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+			    if (event.target == modal) {
+			        modal.style.display = "none";
+			    }
 			}
-		});
-	}
+		}
+	});
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
 	});
+
+	app.post("/api/friends", function(req, res) {
+		var newFriend = req.body;
+		console.log("New Friend: " + newFriend);
+		friends.push(newFriend);
+	});
 }
